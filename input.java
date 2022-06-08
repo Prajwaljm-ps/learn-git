7844cb448137b4f6fdc6a13d0031c07500d84bd9
Prajwal J M <prajwal.jm@publicissapient.com>
2022-06-08 16:05:57 +0530

    uploading input.java

---
+++ input.java
@@ -0,0 +1,17 @@
+package Basics;
+
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word = sc.nextLine();
+        System.out.println(word);
+
+        int a = sc.nextInt();
+        System.out.println("a = "+a);
+        int b = sc.nextInt();
+        System.out.println("b = "+b);
+
+    }
+}
